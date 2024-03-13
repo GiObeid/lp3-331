9b09c6871f7d5cac4bfcdbbd180eb441a467b627
GiObeid <126599102+GiObeid@users.noreply.github.com>
2024-03-13 15:11:03 -0300

    Add files via upload

---
+++ lp3-ifsp/02-funcoes_python/01-intro-funcoes.py
@@ -0,0 +1,25 @@
+#Função
+#modularizar o programa
+# reuso
+#manutenabilidade
+
+#só pode acessar se a hora estiver entre 8h e 18hrs
+hora_atual = 12
+
+if hora_atual >=8 and hora_atual <= 18 :
+    print('permitido o acesso')
+
+
+# entrada = hora atual
+# saida se está dentro ou não do horário comercial
+def dentro_horario_comercial(hora) :
+    if hora_atual >= 8 and hora_atual <= 8 :
+
+
+#Declaração
+ 
+
+# Parametro e Argumento 
+# Chamada
+# Função sem retorno
+# Função com retorno
